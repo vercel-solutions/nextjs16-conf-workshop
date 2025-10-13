f350fa41378cebb6dfcf8558701fb63bab2b03c1
goncy <gonzalo.pozzo4@gmail.com>
2025-10-13 12:09:51 -0300

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
-    cacheComponents: false,
+    cacheComponents: true,
   },
   logging: {
     fetches: {
